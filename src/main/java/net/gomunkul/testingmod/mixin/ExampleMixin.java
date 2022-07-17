6a19319b1a7ec0287299f60531bfd9762901ad78
Stran <Stranniy4el@yandex.ru>
2022-07-17 23:55:23 +0300

    Initial commit

---
+++ src/main/java/net/gomunkul/testingmod/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package net.gomunkul.testingmod.mixin;
+
+
+import net.gomunkul.testingmod.TestingMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TestingMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
