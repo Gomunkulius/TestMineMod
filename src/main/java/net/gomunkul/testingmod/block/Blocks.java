4b83f89a80234de72a64ed36f070ffe4f16a4cb6
Stran <Stranniy4el@yandex.ru>
2022-07-18 14:29:26 +0300

    1.02.2 ADD BLOCKS

--- src/main/java/net/gomunkul/testingmod/block/Blocks.java
+++ src/main/java/net/gomunkul/testingmod/block/Blocks.java
@@ -4,10 +4,6 @@ import net.gomunkul.testingmod.TestingMod;
 
 public class Blocks {
 
-
-
-
-
     public static void registerModBlocks(){
         TestingMod.LOGGER.debug("Registering Mod Blocks for" + TestingMod.MOD_ID);
     }
