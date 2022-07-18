689b0458dcdcd69689ca0ef2de89edd79348dc1f
Stran <Stranniy4el@yandex.ru>
2022-07-18 11:23:54 +0300

    Commit 2

--- src/main/java/net/gomunkul/testingmod/TestingMod.java
+++ src/main/java/net/gomunkul/testingmod/TestingMod.java
@@ -1,6 +1,7 @@
 package net.gomunkul.testingmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.gomunkul.testingmod.items.Item1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,8 @@ public class TestingMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "testingmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +18,6 @@ public class TestingMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Item1.registratingModItems();
 	}
 }
--- src/main/java/net/gomunkul/testingmod/items/Item1.java
+++ src/main/java/net/gomunkul/testingmod/items/Item1.java
@@ -1,4 +1,22 @@
 package net.gomunkul.testingmod.items;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.gomunkul.testingmod.TestingMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class Item1 {
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item( new FabricItemSettings().group(ItemGroup.MISC)));
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(TestingMod.MOD_ID, name), item);
+    }
+
+
+    public static void registratingModItems(){
+        TestingMod.LOGGER.info("Registering Mod Items for" + TestingMod.MOD_ID);
+
+    }
 }
